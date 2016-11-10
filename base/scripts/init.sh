0840aadb6d452932b0ff20c6a44626413b294178
Xiangjun Huang <Xiangjun.Huang@noisuf.com>
2016-11-10 01:47:30 -0800

    add cnpm

--- base/scripts/init.sh
+++ base/scripts/init.sh
@@ -2,5 +2,6 @@ set -e
 
 bash $METEORD_DIR/lib/install_base.sh
 bash $METEORD_DIR/lib/install_node.sh
+bash $METEORD_DIR/lib/install_cnpm.sh
 bash $METEORD_DIR/lib/install_phantomjs.sh
 bash $METEORD_DIR/lib/cleanup.sh
--- base/scripts/run_app.sh
+++ base/scripts/run_app.sh
@@ -1,12 +1,10 @@
 set -e
 
-npm config set registry https://registry.npm.taobao.org
-
 if [ -d /bundle ]; then
   cd /bundle
   tar xzf *.tar.gz
   cd /bundle/bundle/programs/server/
-  npm i
+  cnpm i
   cd /bundle/bundle/
 elif [[ $BUNDLE_URL ]]; then
   cd /tmp
